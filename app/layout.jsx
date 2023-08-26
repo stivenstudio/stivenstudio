7c6b7c2a4fbb3cdb41a2e18d782d7f8b2f0bfbab
stivenstudio <stivenstudiodigital@gmail.com>
2023-08-26 17:16:51 -0500

    uga

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,14 +15,14 @@ export default function RootLayout({ children }) {
       <head>
         <Script
           strategy='afterInteractive'
-          src={`https://www.googletagmanager.com/gtag/js?id=G-79KXEWVSG1`} />
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
         <Script strategy='afterInteractive'>
           {`
           window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments)}
             gtag('js', new Date());
 
-            gtag('config', 'G-79KXEWVSG1');
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
           `}
         </Script>
       </head>
