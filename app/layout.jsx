83f6d941bda58a18634d3cf8de36960a851b51b8
stivenstudio <stivenstudiodigital@gmail.com>
2023-08-26 16:31:59 -0500

    add google analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import './globals.css'
 
 export const metadata = {
@@ -11,8 +12,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      </head>
       <body>
         {children}
+        {/* {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} */}
+        <Script
+          strategy='afterInteractive'
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+        <Script strategy='afterInteractive'>
+          {`
+          window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments)}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          `}
+        </Script>
       </body>
     </html>
   )
